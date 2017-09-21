f65c73a002d1d6fc627991891c81666e7fc24ee7
Alan Ciccotelli <alanciccotelli9@gmail.com>
2017-09-21 23:45:48 +0100

    html changes and script build

--- designs.js
+++ designs.js
@@ -1,10 +1,47 @@
-// Select color input
-// Select size input
+$(document).ready(function() {
 
-// When size is submitted by the user, call makeGrid()
+    /**
+     * @description Builds the grid
+     * @constructor
+     * @param {number} height - The height of the grid
+     * @param {number} width - The width of the grid
+     */
+    function makeGrid(height, width) {
+        let column = 1;
+        while (column <= height) {
+            $('#pixel_canvas').append('<tr class="column' + column + '">');
+            for (let row = 1; row <= width; row++) {
+                $('.column' + column + '').append('<td class="row c' + column + 'r' + row + '">');
+            }
+            column++;
+        }
+    }
 
-function makeGrid() {
+    /**
+     * @description Picks the colour
+     */
+    function colorise() {
+        $('.row').on('click', function() {
+            const colour = $('#colorPicker').val();
+            $(this).css('background', colour);
+        });
+    }
 
-// Your code goes here!
+    /**
+     * @description
+     * When the user changes a number on any
+     * of the fields, the grid will refresh
+     * with the new values
+     */
+    $('#submit').on('click', function(e) {
+        $('#pixel_canvas').children().remove();
+        let gridHeight = $('#input_height').val();
+        let gridWidth = $('#input_width').val();
+        makeGrid(gridHeight, gridWidth);
+        colorise();
+    });
 
-}
+    //init
+    makeGrid(1,1);
+    colorise();
+});
